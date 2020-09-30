71c92b94a2f6039a3a80604ac9200cfbc45f2253
Juan Mora <juan@nimble.gt>
2020-09-30 12:37:58 -0600

    Modify bash scripts

--- aliases.sh
+++ aliases.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
 #############################################################
-# From https://github.com/mig82/visualizer-and-git          #
+# From https://github.com/JPaulMora/visualizer-and-git      #
+# Original https://github.com/mig82/visualizer-and-git      #
 # @Author: Miguelangel Fernandez                            #
 #############################################################
 
-# To fire your Git client of choice or the Terminal from Visualizer's `Window>Version` Control menu option.
+# To fire up your Git client of choice or the Terminal from Visualizer's `Window>Version` Control menu option.
+# these work on macOS, not tested.
+
 #git config --global --add alias.gui '!sh -c /usr/local/git/libexec/git-core/git-gui'
 #git config --global --add alias.gui '!sh -c /usr/local/Cellar/git/2.16.3/libexec/git-core/git-gui'
 #git config --global --add alias.gui 'open -a SourceTree .'
 #git config --global --add alias.gui '/Applications/GitKraken.app/Contents/MacOS/GitKraken --path .'
-git config --global alias.gui 'open -a Terminal .'
+#git config --global alias.gui 'open -a Terminal .'
+
+# Useful aliases for terminal junkies
 
 # To temporarily quiet down changes to specific files without altering your .gitignore.
 git config --global alias.mute 'update-index --assume-unchanged'
@@ -18,8 +23,5 @@ git config --global alias.unmute 'update-index --no-assume-unchanged'
 
 # To create a zip archive of your project.
 git config --global alias.zip '!git clean -fXd && git archive --format zip -9 HEAD --output $1 && ls -lh $1'
-
-# To download the .gitingore and .gitattributes into the current localtion.
-git config --global alias.visinit '!curl -L http://bit.ly/visgitignore -o .gitignore && curl -L http://bit.ly/visgitattributes -o .gitattributes'
 
 echo "Done: Git aliases for Visualizer"
--- filters.sh
+++ filters.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
 
 #############################################################
-# From https://github.com/mig82/visualizer-and-git          #
+# From https://github.com/JPaulMora/visualizer-and-git      #
+# Original https://github.com/mig82/visualizer-and-git      #
 # @Author: Miguelangel Fernandez                            #
 #############################################################
 
 # Run this script in order to define the filters that will prettify some of the
 # files in your visualizer project and ignore the non-important metadata in some
-# of the project files. The --global flag ensures you only have to do this once
-# foir all your projects.
+# other project files.
 
 # Sort projectprop.xml and remove the lastmodifiedtime element which keeps changing.
-git config --global filter.kony_projectprop.clean "(head -n 1 %f; cat %f | grep attributes | sed /lastmodifiedtime/d | sort --ignore-case; tail -n -1 %f; echo;)"
+git config filter.kony_projectprop.clean "(head -n 1 %f; cat %f | grep attributes | sed /lastmodifiedtime/d | sort --ignore-case; tail -n -1 %f; echo;)"
 
 # Avoid pushing your Fabric password to source control.
-git config --global filter.kony_headless_props.clean 'sed -e "s/\(.*password.*=\).*/\1/"'
+git config filter.kony_headless_props.clean 'sed -e "s/\(.*password.*=\).*/\1/"'
 
 # Ignore blank lines -typically in JS files.
-git config --global filter.rm_whiteline.clean 'sed -e "s/^[[:blank:]]+$//"'
+git config filter.rm_whiteline.clean 'sed -e "s/^[[:blank:]]+$//"'
 
 # Prettify and sort projectProperties.json and ignore the non-important keys which keep changing.
-git config --global filter.kony_projectProperties.clean "jq --indent 4 --sort-keys -j 'del(.nativeContainerProperties.isModified)'"
+git config filter.kony_projectProperties.clean "jq --indent 4 --sort-keys -j 'del(.nativeContainerProperties.isModified)'"
 
 # Prettify and sort all views -i.e. forms, templates, reusable components and pop-ups and ignore WYSIWYG metadata.
-git config --global filter.kony_views.clean "jq --indent 4 -j 'del(.device, .prevDevice, .platform, .prevPlatform, .shellCSS, .context.platform, .currentLocale, .osPlatform, .osVersion, .globalUserWidgetRefs, .context.formID)'"
+git config filter.kony_views.clean "jq --indent 4 -j 'del(.device, .prevDevice, .platform, .prevPlatform, .shellCSS, .context.platform, .currentLocale, .osPlatform, .osVersion, .globalUserWidgetRefs, .context.formID)'"
 
 # Prettify and sort generic JSON files such as mobileFabricServiceConfigMap.json and objectServicesConfigMap.json
-git config --global filter.pretty_json.clean "jq --indent 4 --sort-keys ."
+git config filter.pretty_json.clean "jq --indent 4 --sort-keys ."
 
 # Version nativeapi.json but not the properties inside it that store absolute paths.
-git config --global filter.kony_native_api_json.clean "jq --indent 4 -j 'del(..|(.url?,.filepath?))'"
+git config filter.kony_native_api_json.clean "jq --indent 4 -j 'del(..|(.url?,.filepath?))'"
 
 echo "Done: Git filters for Visualizer"
